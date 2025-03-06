6d44991f49d9b72ad82e606fb35c1fe20729e1da
minhi0449 <minhi0449@gmail.com>
2025-03-06 16:27:53 +0900

    chore: pay home 화면 레이아웃 수정

--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\SDKs\flutter_windows_3.24.3-stable\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\workspace\APPlusMarket_Flutter"
+export "FLUTTER_ROOT=/Users/kimminimanimu/Developments/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/kimminimanimu/APPlusMarket_Flutter"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
